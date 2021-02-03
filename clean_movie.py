a7532081ca62fdea3064f0f1e5f570a7d4ae6a65
Edward Rivera <72109265+ed12rivera@users.noreply.github.com>
2021-02-03 01:05:07 -0800

    Clean some of the functions.

---
+++ clean_movie.py
@@ -0,0 +1,40 @@
+def clean_movie(movie):
+    movie = dict(movie) # Create a non-destructive copy
+    alt_titles = {}
+    # combine alternate titles into one list
+    for key in ['Also known as','Arabic','Cantonese','Chinese','French',
+                'Hangul','Hebrew','Hepburn','Japanese','Literally',
+                'Mandarin','McCune–Reischauer','Original title','Polish',
+                'Revised Romanization','Romanized','Russian',
+                'Simplified','Traditional','Yiddish']:
+        if key in movie:
+            alt_titles[key] = movie[key]
+            movie.pop(key)
+        if len(alt_titles) > 0:
+            movie['alt titles'] = alt_titles
+            
+    # merge column names
+    def change_column_name(old_name, new_name):
+        if old_name in movie:
+            movie[new_name] = movie.pop(old_name)
+    change_column_name('Adaptation by', 'Writer(s)')
+    change_column_name('Country of origin', 'Country')
+    change_column_name('Directed by', 'Director')
+    change_column_name('Distributed by', 'Distributor')
+    change_column_name('Edited by', 'Editor(s)')
+    change_column_name('Length', 'Running time')
+    change_column_name('Original release', 'Release date')
+    change_column_name('Music by', 'Composer(s)')
+    change_column_name('Produced by', 'Producer(s)')
+    change_column_name('Producer', 'Producer(s)')
+    change_column_name('Productioncompanies ', 'Production company(s)')
+    change_column_name('Productioncompany ', 'Production company(s)')
+    change_column_name('Released', 'Release Date')
+    change_column_name('Release Date', 'Release date')
+    change_column_name('Screen story by', 'Writer(s)')
+    change_column_name('Screenplay by', 'Writer(s)')
+    change_column_name('Story by', 'Writer(s)')
+    change_column_name('Theme music composer', 'Composer(s)')
+    change_column_name('Written by', 'Writer(s)')
+    
+    return movie
